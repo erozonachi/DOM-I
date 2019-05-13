bf30f0b2ecfab407bee2a02c4a3d80d805b966b1
erozonachi <erozonachi.james@gmail.com>
2019-05-13 14:08:04 +0100

    Update the html with the json data
    Use selectors to update the content to match the example file, also
    remember to update the src attributes on images.

--- js/index.js
+++ js/index.js
@@ -38,19 +38,50 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+// navigation
 const logo = document.querySelector("#logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"]);
+logo.src = siteContent["nav"]["img-src"];
 
 const navItems = document.querySelectorAll(`nav > a`);
+navItems.forEach((item, pos) => item.textContent = siteContent.nav[`nav-item-${pos+1}`]);
 
-const ctaTexts = document.querySelector(`.cta-text`).childNodes;
+// cta Section
+const ctaTexts = document.querySelector(`.cta-text`).children;
+const headText = siteContent[`cta`][`h1`].split(` `).join(`<br>`);
+ctaTexts[0].innerHTML = headText;
+ctaTexts[1].textContent = siteContent[`cta`][`button`];
+
 const ctaImg = document.querySelector(`#cta-img`);
+ctaImg.src = siteContent[`cta`][`img-src`];
 
+// main content - top
 const topTexts = document.querySelectorAll(`.top-content > .text-content`);
+topTexts[0].children[0].textContent = siteContent[`main-content`][`features-h4`];
+topTexts[0].children[1].textContent = siteContent[`main-content`][`features-content`];
+topTexts[1].children[0].textContent = siteContent[`main-content`][`about-h4`];
+topTexts[1].children[1].textContent = siteContent[`main-content`][`about-content`];
+
+// main content - middle image
 const midImg = document.querySelector(`#middle-img`);
+midImg.src = siteContent[`main-content`][`middle-img-src`];
+
+// main content - bottom
 const bottomTexts = document.querySelectorAll(`.bottom-content > .text-content`);
+bottomTexts[0].children[0].textContent = siteContent[`main-content`][`services-h4`];
+bottomTexts[0].children[1].textContent = siteContent[`main-content`][`services-content`];
+bottomTexts[1].children[0].textContent = siteContent[`main-content`][`product-h4`];
+bottomTexts[1].children[1].textContent = siteContent[`main-content`][`product-content`];
+bottomTexts[2].children[0].textContent = siteContent[`main-content`][`vision-h4`];
+bottomTexts[2].children[1].textContent = siteContent[`main-content`][`vision-content`];
 
-const contactHeading = document.querySelector(`.contact > h4`);
-const contactItems = document.querySelectorAll(`.contact > p`);
+// contact section
+const contactItems = document.querySelector(`.contact`);
+contactItems.children[0].textContent = siteContent[`contact`][`contact-h4`];
+const address = siteContent[`contact`][`address`].replace(`t `, `t<br>`);
+contactItems.children[1].innerHTML = address;
+contactItems.children[2].textContent = siteContent[`contact`][`phone`];
+contactItems.children[3].textContent = siteContent[`contact`][`email`];
 
+// footer
 const footerText = document.querySelector(`footer > p`);
+footerText.innerHTML = siteContent[`footer`][`copyright`];
